3e7cb5129fcb25613d51453a4dde0d72d316492c
El kaabaoui Soufiane <soufiane.elkaabaoui@gmail.com>
2024-07-16 17:24:23 +0100

    setting returns 12072024

--- app/Enums/SettingEnum.php
+++ app/Enums/SettingEnum.php
@@ -7,6 +7,7 @@ use Spatie\Enum\Laravel\Enum;
 /**
  * @method static self GLOBAL_STOCK_NOTIF()
  * @method static self BOOKING_CALENDAR_COLORS()
+ * @method static self CHILDREN_DISCOUNT()
  */
 final class SettingEnum extends Enum
 {
